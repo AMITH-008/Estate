ebcb7c25f056fe5a119e4e1fbd23df2c28c9f827
amith <amithkouti@gmail.com>
2023-11-22 09:45:18 +0530

    Connected to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 
 const app = express();
 
+
+//Connect to MongoDB using mongoose(ORM)
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log("Connected to database");
+}).catch((err) => console.log(err))
 
 app.listen(5173, () => {
     console.log(`Server running on ${5173}`)
