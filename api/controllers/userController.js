4e6caaf8db355cb6917df7897d0c9b296e76fc7f
amith <amithkouti@gmail.com>
2023-11-22 14:03:18 +0530

    Created a test api route

---
+++ api/controllers/userController.js
@@ -0,0 +1,3 @@
+export const test = (request, response) => {
+    response.send("Hello World from test user");
+}
--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/userRoute.js';
 
 dotenv.config();
 
 const app = express();
 
-
+app.use('/api/user', userRouter);
 //Connect to MongoDB using mongoose(ORM)
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log("Connected to database");
@@ -14,4 +15,8 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 
 app.listen(5173, () => {
     console.log(`Server running on ${5173}`)
+})
+
+app.get('/', (request, response) => {
+    response.send(request)
 })
