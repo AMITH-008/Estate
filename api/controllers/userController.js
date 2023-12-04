6b0b1b553361f96b0a10f9c50b23e8f73342edb0
amith <amithkouti@gmail.com>
2023-12-04 23:32:35 +0530

    Created update user API route

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,3 +1,32 @@
+import User from "../models/userModel.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from 'bcryptjs';
+
 export const test = (request, response) => {
     response.send("Hello World from test user");
+}
+
+export const updateUser = async (request, response, next) => {
+    if (request.user._id != request.params.id) {
+        return next(errorHandler(401, "You can update only your account!!"));
+    }
+    try{
+        if (request.body.password) {
+            request.body.password = bcryptjs.hashSync(request.body.password, 10);
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(request.params.id,  {
+            $set: {
+                username: request.body.username,
+                email : request.body.email,
+                password: request.body.password,
+                profileImg : request.body.profileImg
+            }
+
+        }, {new:true} );
+        const {password, ...rest } = updatedUser._doc;
+        response.status(200).json(rest);
+    }catch(error) {
+        next(error);
+    }
 }
--- api/index.js
+++ api/index.js
@@ -3,11 +3,13 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/userRoute.js';
 import authRouter from './routes/authRouter.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json())
+app.use(cookieParser());
 
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
